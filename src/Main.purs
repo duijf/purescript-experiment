75d51207a1c657af9309ca0d3c6b0bbe659af2e3
Arian van Putten <arian.vanputten@gmail.com>
2019-01-12 15:05:58 +0100

    Ping Pong

--- src/Main.purs
+++ src/Main.purs
@@ -8,11 +8,15 @@ import Concur.React (HTML)
 import Concur.React.DOM (button, text)
 import Concur.React.Run (runWidgetInDom)
 import Concur.React.Props (onClick)
+import Control.Monad.Rec.Class (forever)
 import Control.Alt ((<|>))
 
 
 contactSearch :: forall a. Widget HTML a
-contactSearch = (button [onClick] [text "click me"] >>= \_ -> text "BOOM") <|> text "yes"
+contactSearch = forever $ do
+  _ <- button [onClick] [text "ping"]
+  button [onClick] [text "pong"]
+  -- (button [onClick] [text "click me"] >>= \_ -> text "BOOM") <|> text "yes"
 
 
 main :: Effect Unit
