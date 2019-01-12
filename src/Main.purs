8996c5ce5b2713ca61cd0780b9ed530e7a1579c7
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2019-01-12 13:42:45 +0100

    Add basic Concur widgets
    This doens't work yet as Concur seems to use react. We need
    to set up a JS module bundler.

--- src/Main.purs
+++ src/Main.purs
@@ -3,7 +3,16 @@ module Main where
 import Prelude
 
 import Effect (Effect)
---import Effect as E
+import Concur.Core (Widget)
+import Concur.React (HTML)
+import Concur.React.DOM (text)
+import Concur.React.Run (runWidgetInDom)
+
+
+contactSearch :: forall a. Widget HTML a
+contactSearch = text "HI IT WORKS"
+
 
 main :: Effect Unit
-main = pure unit
+main = do
+  runWidgetInDom "contact" contactSearch
