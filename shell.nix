9ae01087c44fe7ffc398605b32521346259f423d
Arian van Putten <arian.vanputten@gmail.com>
2019-01-12 11:48:28 +0100

    Nix Shell

---
+++ shell.nix
@@ -0,0 +1,13 @@
+let
+  pkgs = import <nixpkgs> {};
+
+  easyPS = import (pkgs.fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "993f63359b64db080061b274e4688e3b80c4f68e";
+    sha256 = "18b7fmmxkg38y1av9kfgcv2rikdlji51ya5b9p7sy3aml2hprmi5";
+  });
+in pkgs.stdenv.mkDerivation {
+  name = "yo";
+  buildInputs = with easyPS; [ purs psc-package-simple ];
+}
